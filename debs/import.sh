6f37fa59e2051f711afc87d04e0dec275fb0d4e3
pw5a29 <pw5a29@gmail.com>
2015-06-19 20:06:37 +0800

    updated repo

--- debs/import.sh
+++ debs/import.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# Pushes DEBs to the Packages file and bzips it up; all I need to do is push to GitHub after this
-
-dpkg-scanpackages debs / > Packages
-
-bzip2 -fks Packages
+dpkg-scanpackages . /dev/null > Packages && bzip2 < Packages > Packages.bz2 && gzip < Packages > Packages.gz
